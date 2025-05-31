4cbd6568becd6ce6aef4a96bbc90e2f20bec2db4
Johannes Stein <johannesstein@freeze-dev.com>
2025-05-31 21:40:18 +0100

    Add optional value

--- units/Goccia.Values.Base.pas
+++ units/Goccia.Values.Base.pas
@@ -17,11 +17,74 @@ type
     function TypeName: string; virtual; abstract;
   end;
 
+  TGocciaOptionalValue<T: TGocciaValue> = class(TGocciaValue)
+  private
+    function GetIsUndefined: Boolean;
+  protected
+    FValue: T;
+  public
+    constructor Create(AValue: T);
+
+    function ToString: string; override;
+    function ToBoolean: Boolean; override;
+    function ToNumber: Double; override;
+    function TypeName: string; override;
+
+    property Value: T read FValue;
+    property IsUndefined: Boolean read GetIsUndefined;
+  end;
+
 implementation
+
+uses
+  Goccia.Values.Undefined;
 
 function TGocciaValue.ToBoolean: Boolean;
 begin
   Result := True;
+end;
+
+constructor TGocciaOptionalValue<T>.Create(AValue: T);
+begin
+  inherited Create;
+  FValue := AValue;
+end;
+
+function TGocciaOptionalValue<T>.GetIsUndefined: Boolean;
+begin
+  Result := FValue = nil;
+end;
+
+function TGocciaOptionalValue<T>.ToBoolean: Boolean;
+begin
+  if GetIsUndefined then
+    Result := TGocciaUndefinedValue.Create.ToBoolean
+  else
+    Result := FValue.ToBoolean;
+end;
+
+function TGocciaOptionalValue<T>.ToNumber: Double;
+begin
+  if GetIsUndefined then
+    Result := TGocciaUndefinedValue.Create.ToNumber
+  else
+    Result := FValue.ToNumber;
+end;
+
+function TGocciaOptionalValue<T>.TypeName: string;
+begin
+  if GetIsUndefined then
+    Result := TGocciaUndefinedValue.Create.TypeName
+  else
+    Result := FValue.TypeName;
+end;
+
+function TGocciaOptionalValue<T>.ToString: string;
+begin
+  if GetIsUndefined then
+    Result := TGocciaUndefinedValue.Create.ToString
+  else
+    Result := FValue.ToString;
 end;
 
 end.
