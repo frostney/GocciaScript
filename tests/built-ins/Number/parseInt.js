2dd9d3c5b9b0b7718c87a9882690708615451382
Johannes Stein <johannesstein@freeze-dev.com>
2025-06-22 21:51:01 +0100

    Improve `Number` built-in static methods

--- tests/built-ins/Number/parseInt.js
+++ tests/built-ins/Number/parseInt.js
@@ -9,10 +9,8 @@ test("Number.parseInt", () => {
   expect(Number.parseInt("123abc")).toBe(123);
 
   expect(Number.parseInt("0x1A")).toBe(26);
-  expect(Number.parseInt("0b1010")).toBe(10);
-  expect(Number.parseInt("0o12")).toBe(10);
   expect(Number.parseInt("123", 10)).toBe(123);
   expect(Number.parseInt("123", 8)).toBe(83);
   expect(Number.parseInt("123", 16)).toBe(291);
-  expect(Number.parseInt("123", 2)).toBe(5);
+  expect(Number.parseInt("123", 2)).toBe(1);
 });
--- units/Goccia.Builtins.GlobalNumber.pas
+++ units/Goccia.Builtins.GlobalNumber.pas
@@ -43,37 +43,266 @@ begin
 end;
 
 function TGocciaGlobalNumber.NumberParseInt(Args: TObjectList<TGocciaValue>; ThisValue: TGocciaValue): TGocciaValue;
+var
+  InputStr: string;
+  Radix: Integer;
+  I, StartPos: Integer;
+  C: Char;
+  Sign: Integer;
+  ResultValue: Int64;
+  ValidChars: string;
 begin
-  // TODO: Use parseInt
-  Result := TGocciaNumberValue.Create(StrToInt(Args[0].ToString));
+  if Args.Count = 0 then
+  begin
+    Result := TGocciaNumberValue.Create(NaN);
+    Exit;
+  end;
+
+  InputStr := Trim(Args[0].ToString);
+  if InputStr = '' then
+  begin
+    Result := TGocciaNumberValue.Create(NaN);
+    Exit;
+  end;
+
+  // Get radix
+  if Args.Count > 1 then
+    Radix := Trunc(Args[1].ToNumber)
+  else
+    Radix := 0; // 0 means auto-detect
+
+  I := 1;
+  Sign := 1;
+
+  // Check for sign
+  if (I <= Length(InputStr)) and (InputStr[I] = '-') then
+  begin
+    Sign := -1;
+    Inc(I);
+  end
+  else if (I <= Length(InputStr)) and (InputStr[I] = '+') then
+  begin
+    Inc(I);
+  end;
+
+  // Auto-detect radix when radix is 0 or unspecified
+  if (Radix = 0) then
+  begin
+    if (I + 1 <= Length(InputStr)) and (InputStr[I] = '0') and
+       ((InputStr[I + 1] = 'x') or (InputStr[I + 1] = 'X')) then
+    begin
+      Radix := 16;
+      Inc(I, 2); // Skip "0x" or "0X"
+    end
+    else
+      Radix := 10;
+  end
+  // Special case for radix 16 with 0x prefix (even when radix explicitly specified)
+  else if (Radix = 16) and (I + 1 <= Length(InputStr)) and (InputStr[I] = '0') and
+          ((InputStr[I + 1] = 'x') or (InputStr[I + 1] = 'X')) then
+  begin
+    Inc(I, 2); // Skip "0x" or "0X"
+  end;
+
+  // Check for invalid radix
+  if (Radix < 2) or (Radix > 36) then
+  begin
+    Result := TGocciaNumberValue.Create(NaN);
+    Exit;
+  end;
+
+  StartPos := I; // Remember where digit parsing starts
+
+  // Build valid characters for this radix
+  ValidChars := '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  ValidChars := Copy(ValidChars, 1, Radix);
+
+  ResultValue := 0;
+
+  // Parse digits
+  while I <= Length(InputStr) do
+  begin
+    C := UpCase(InputStr[I]);
+    if Pos(C, ValidChars) = 0 then
+      Break; // Stop at first invalid character
+
+    ResultValue := ResultValue * Radix + (Pos(C, ValidChars) - 1);
+    Inc(I);
+  end;
+
+  // If no digits were parsed, return NaN
+  if I = StartPos then // No digits parsed
+  begin
+    Result := TGocciaNumberValue.Create(NaN);
+    Exit;
+  end;
+
+  Result := TGocciaNumberValue.Create(Sign * ResultValue);
 end;
 
 function TGocciaGlobalNumber.NumberParseFloat(Args: TObjectList<TGocciaValue>; ThisValue: TGocciaValue): TGocciaValue;
+var
+  InputStr: string;
+  I: Integer;
+  C: Char;
+  Sign: Integer;
+  IntegerPart, FractionalPart: Double;
+  FractionDivisor: Double;
+  HasDigits: Boolean;
 begin
-  // TODO: Use parseFloat
-  Result := TGocciaNumberValue.Create(StrToFloat(Args[0].ToString));
+  if Args.Count = 0 then
+  begin
+    Result := TGocciaNumberValue.Create(NaN);
+    Exit;
+  end;
+
+  InputStr := Trim(Args[0].ToString);
+  if InputStr = '' then
+  begin
+    Result := TGocciaNumberValue.Create(NaN);
+    Exit;
+  end;
+
+  I := 1;
+  Sign := 1;
+  HasDigits := False;
+
+  // Check for sign
+  if (I <= Length(InputStr)) and (InputStr[I] = '-') then
+  begin
+    Sign := -1;
+    Inc(I);
+  end
+  else if (I <= Length(InputStr)) and (InputStr[I] = '+') then
+  begin
+    Inc(I);
+  end;
+
+  IntegerPart := 0;
+
+  // Parse integer part
+  while I <= Length(InputStr) do
+  begin
+    C := InputStr[I];
+    if (C >= '0') and (C <= '9') then
+    begin
+      IntegerPart := IntegerPart * 10 + (Ord(C) - Ord('0'));
+      HasDigits := True;
+      Inc(I);
+    end
+    else
+      Break;
+  end;
+
+  FractionalPart := 0;
+  FractionDivisor := 1;
+
+  // Check for decimal point
+  if (I <= Length(InputStr)) and (InputStr[I] = '.') then
+  begin
+    Inc(I);
+
+    // Parse fractional part
+    while I <= Length(InputStr) do
+    begin
+      C := InputStr[I];
+      if (C >= '0') and (C <= '9') then
+      begin
+        FractionDivisor := FractionDivisor * 10;
+        FractionalPart := FractionalPart * 10 + (Ord(C) - Ord('0'));
+        HasDigits := True;
+        Inc(I);
+      end
+      else
+        Break;
+    end;
+  end;
+
+  // If no digits were parsed, return NaN
+  if not HasDigits then
+  begin
+    Result := TGocciaNumberValue.Create(NaN);
+    Exit;
+  end;
+
+  Result := TGocciaNumberValue.Create(Sign * (IntegerPart + (FractionalPart / FractionDivisor)));
 end;
 
 function TGocciaGlobalNumber.NumberIsFinite(Args: TObjectList<TGocciaValue>; ThisValue: TGocciaValue): TGocciaValue;
+var
+  NumberArg: TGocciaNumberValue;
+  Value: Double;
 begin
-  // TODO: Use isfinite
-  Result := TGocciaBooleanValue.Create(not IsInfinite(Args[0].ToNumber));
+  if Args.Count = 0 then
+  begin
+    Result := TGocciaBooleanValue.Create(False);
+    Exit;
+  end;
+
+  NumberArg := TGocciaNumberValue(Args[0]);
+
+  if not (NumberArg is TGocciaNumberValue) then
+  begin
+    Result := TGocciaBooleanValue.Create(False);
+    Exit;
+  end;
+
+  Value := NumberArg.Value;
+
+  if IsNaN(Value) or IsInfinite(Value) then
+  begin
+    Result := TGocciaBooleanValue.Create(False);
+    Exit;
+  end;
+
+  Result := TGocciaBooleanValue.Create(True);
 end;
 
 function TGocciaGlobalNumber.NumberIsNaN(Args: TObjectList<TGocciaValue>; ThisValue: TGocciaValue): TGocciaValue;
 begin
-  // TODO: Use isnan
+  if Args.Count = 0 then
+  begin
+    Result := TGocciaBooleanValue.Create(False);
+    Exit;
+  end;
+
+  if not (Args[0] is TGocciaNumberValue) then
+  begin
+    Result := TGocciaBooleanValue.Create(False);
+    Exit;
+  end;
+
   Result := TGocciaBooleanValue.Create(IsNaN(Args[0].ToNumber));
 end;
 
 function TGocciaGlobalNumber.NumberIsInteger(Args: TObjectList<TGocciaValue>; ThisValue: TGocciaValue): TGocciaValue;
 var
-  Value: Integer;
+  NumberArg: TGocciaNumberValue;
+  Value: Double;
 begin
-  if TryStrToInt(Args[0].ToString, Value) then
-    Result := TGocciaBooleanValue.Create(True)
-  else
+  if Args.Count = 0 then
+  begin
     Result := TGocciaBooleanValue.Create(False);
+    Exit;
+  end;
+
+  NumberArg := TGocciaNumberValue(Args[0]);
+
+  if not (NumberArg is TGocciaNumberValue) then
+  begin
+    Result := TGocciaBooleanValue.Create(False);
+    Exit;
+  end;
+
+  Value := NumberArg.Value;
+
+  if IsNaN(Value) or IsInfinite(Value) then
+  begin
+    Result := TGocciaBooleanValue.Create(False);
+    Exit;
+  end;
+
+  Result := TGocciaBooleanValue.Create(Value = Trunc(Value));
 end;
 
 
