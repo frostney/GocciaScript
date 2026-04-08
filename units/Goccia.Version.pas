634c14be195920bf39d0c40cad204257d5cf7349
Johannes Stein <johannesstein@freeze-dev.com>
2026-04-08 10:25:45 +0100

    Use baked version metadata in all builds
    - Generate version/commit constants in CI for every build
    - Remove the runtime git probe from `Goccia.Version`

--- units/Goccia.Version.pas
+++ units/Goccia.Version.pas
@@ -9,8 +9,6 @@ function GetCommit: string;
 
 implementation
 
-{$IFDEF PRODUCTION}
-
 {$I Goccia.Version.Generated.inc}
 
 function GetVersion: string;
@@ -22,71 +20,5 @@ function GetCommit: string;
 begin
   Result := BAKED_COMMIT;
 end;
-
-{$ELSE}
-
-uses
-  Process,
-  SysUtils;
-
-var
-  CachedVersion: string;
-  CachedCommit: string;
-
-function RunGit(const AArgs: array of string): string;
-var
-  Output: string;
-begin
-  if RunCommand('git', AArgs, Output) then
-    Result := Trim(Output)
-  else
-    Result := '';
-end;
-
-function LooksLikeSemver(const AValue: string): Boolean;
-var
-  FirstDot: SizeInt;
-begin
-  FirstDot := Pos('.', AValue);
-  Result := (FirstDot > 1) and (FirstDot < Length(AValue));
-end;
-
-procedure Initialize;
-var
-  Describe: string;
-  DashPos: SizeInt;
-begin
-  Describe := RunGit(['describe', '--tags', '--always']);
-  CachedCommit := RunGit(['rev-parse', '--short', 'HEAD']);
-  if CachedCommit = '' then
-    CachedCommit := 'unknown';
-
-  if (Describe = '') or not LooksLikeSemver(Describe) then
-  begin
-    CachedVersion := '0.0.0-dev';
-    Exit;
-  end;
-
-  DashPos := Pos('-', Describe);
-  if DashPos > 0 then
-    CachedVersion := Copy(Describe, 1, DashPos - 1) + '-dev'
-  else
-    CachedVersion := Describe;
-end;
-
-function GetVersion: string;
-begin
-  Result := CachedVersion;
-end;
-
-function GetCommit: string;
-begin
-  Result := CachedCommit;
-end;
-
-initialization
-  Initialize;
-
-{$ENDIF}
 
 end.
