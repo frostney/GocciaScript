0761f5dedd0c94d3331f5e2639463665ff4d99c4
Johannes Stein <johannesstein@freeze-dev.com>
2026-05-19 22:05:24 +0100

    Fix thenable await rejection handling

--- source/units/Goccia.Values.Await.pas
+++ source/units/Goccia.Values.Await.pas
@@ -12,8 +12,12 @@ function AwaitValue(const AValue: TGocciaValue): TGocciaValue;
 implementation
 
 uses
+  SysUtils,
+
   Goccia.Arguments.Collection,
+  Goccia.Constants.ErrorNames,
   Goccia.Constants.PropertyNames,
+  Goccia.Error,
   Goccia.Error.Messages,
   Goccia.Error.Suggestions,
   Goccia.FetchManager,
@@ -24,7 +28,8 @@ uses
   Goccia.Values.FunctionBase,
   Goccia.Values.NativeFunction,
   Goccia.Values.ObjectValue,
-  Goccia.Values.PromiseValue;
+  Goccia.Values.PromiseValue,
+  Goccia.VM.Exception;
 
 procedure DrainAwaitMicrotasks;
 var
@@ -35,64 +40,97 @@ begin
     Queue.DrainQueue;
 end;
 
+procedure RejectPromiseWithException(const APromise: TGocciaPromiseValue;
+  const AException: Exception);
+begin
+  if AException is EGocciaBytecodeThrow then
+    APromise.Reject(EGocciaBytecodeThrow(AException).ThrownValue)
+  else if AException is TGocciaThrowValue then
+    APromise.Reject(TGocciaThrowValue(AException).Value)
+  else if AException is TGocciaTypeError then
+    APromise.Reject(CreateErrorObject(TYPE_ERROR_NAME, AException.Message))
+  else if AException is TGocciaReferenceError then
+    APromise.Reject(CreateErrorObject(REFERENCE_ERROR_NAME, AException.Message))
+  else if AException is TGocciaSyntaxError then
+    APromise.Reject(CreateErrorObject(SYNTAX_ERROR_NAME, AException.Message))
+  else
+    APromise.Reject(CreateErrorObject(ERROR_NAME, AException.Message));
+end;
+
 // ES2026 §27.7.5.3 Await(value)
 function AwaitValue(const AValue: TGocciaValue): TGocciaValue;
 var
   ThenMethod: TGocciaValue;
   Promise: TGocciaPromiseValue;
+  PromiseRooted: Boolean;
   ThenArgs: TGocciaArgumentsCollection;
 begin
-  if AValue is TGocciaPromiseValue then
-  begin
-    Promise := TGocciaPromiseValue(AValue);
-    if Assigned(TGarbageCollector.Instance) then
-      TGarbageCollector.Instance.AddTempRoot(Promise);
-  end
-  // ES2026 §25.6.4.6.1 PromiseResolve: If value is a thenable (object with
-  // callable .then), wrap it in a Promise via resolve
-  else if AValue is TGocciaObjectValue then
-  begin
-    ThenMethod := AValue.GetProperty(PROP_THEN);
-    if Assigned(ThenMethod) and not (ThenMethod is TGocciaUndefinedLiteralValue) and ThenMethod.IsCallable then
+  Promise := nil;
+  PromiseRooted := False;
+
+  try
+    if AValue is TGocciaPromiseValue then
+    begin
+      Promise := TGocciaPromiseValue(AValue);
+      if Assigned(TGarbageCollector.Instance) then
+      begin
+        TGarbageCollector.Instance.AddTempRoot(Promise);
+        PromiseRooted := True;
+      end;
+    end
+    // ES2026 §25.6.4.6.1 PromiseResolve: If value is a thenable (object with
+    // callable .then), wrap it in a Promise via resolve
+    else if AValue is TGocciaObjectValue then
     begin
       Promise := TGocciaPromiseValue.Create;
       if Assigned(TGarbageCollector.Instance) then
+      begin
         TGarbageCollector.Instance.AddTempRoot(Promise);
-      ThenArgs := TGocciaArgumentsCollection.Create([
-        TGocciaNativeFunctionValue.Create(Promise.DoResolve, 'resolve', 1),
-        TGocciaNativeFunctionValue.Create(Promise.DoReject, 'reject', 1)
-      ]);
+        PromiseRooted := True;
+      end;
+
       try
-        // ES2026 §25.6.4.6.1 step 9: If Call(then, ...) throws, reject promise
-        try
-          TGocciaFunctionBase(ThenMethod).Call(ThenArgs, AValue);
-        except
-          on E: TGocciaThrowValue do
-            Promise.Reject(E.Value);
+        ThenMethod := AValue.GetProperty(PROP_THEN);
+        if Assigned(ThenMethod) and not (ThenMethod is TGocciaUndefinedLiteralValue) and ThenMethod.IsCallable then
+        begin
+          ThenArgs := TGocciaArgumentsCollection.Create([
+            TGocciaNativeFunctionValue.Create(Promise.DoResolve, 'resolve', 1),
+            TGocciaNativeFunctionValue.Create(Promise.DoReject, 'reject', 1)
+          ]);
+          try
+            // ES2026 §25.6.4.6.1 step 9: If Call(then, ...) throws, reject promise
+            try
+              TGocciaFunctionBase(ThenMethod).Call(ThenArgs, AValue);
+            except
+              on E: Exception do
+                RejectPromiseWithException(Promise, E);
+            end;
+          finally
+            ThenArgs.Free;
+          end;
+        end
+        else
+        begin
+          // ES2026 §27.7.5.3 step 2: Await wraps the value in Promise.resolve(),
+          // introducing a microtask boundary even for non-thenable objects
+          DrainAwaitMicrotasks;
+          Result := AValue;
+          Exit;
         end;
-      finally
-        ThenArgs.Free;
+      except
+        on E: Exception do
+          RejectPromiseWithException(Promise, E);
       end;
     end
     else
     begin
       // ES2026 §27.7.5.3 step 2: Await wraps the value in Promise.resolve(),
-      // introducing a microtask boundary even for non-thenable objects
+      // introducing a microtask boundary even for primitive values
       DrainAwaitMicrotasks;
       Result := AValue;
       Exit;
     end;
-  end
-  else
-  begin
-    // ES2026 §27.7.5.3 step 2: Await wraps the value in Promise.resolve(),
-    // introducing a microtask boundary even for primitive values
-    DrainAwaitMicrotasks;
-    Result := AValue;
-    Exit;
-  end;
 
-  try
     if Promise.State <> gpsPending then
     begin
       // ES2026 §27.7.5.3 step 3-4: Even already-settled promises introduce
@@ -114,7 +152,7 @@ begin
     else
       ThrowTypeError(SErrorAwaitPromiseUnsettled, SSuggestAwaitMicrotaskDrain);
   finally
-    if Assigned(TGarbageCollector.Instance) then
+    if PromiseRooted and Assigned(TGarbageCollector.Instance) then
       TGarbageCollector.Instance.RemoveTempRoot(Promise);
   end;
 end;
